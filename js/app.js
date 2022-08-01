c9aaa848187c2711fb983b590a6370177230b362
Clara Tavares <claratavaresilva@gmail.com>
2022-07-31 21:35:58 -0300

    teste

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 (function ($) {
   "use strict";
 
@@ -77,7 +78,6 @@
     ],
   });
 })(jQuery);
-
 //Unique Firebase Object
 var firebaseConfig = {
   apiKey: "AIzaSyAkhTtORnvWoZCOUIvgzC_yAj5azjbuykU",
